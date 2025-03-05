2b4260398b047fe6ff397edf21c1db98c33ebb70
wil974-dev <wggron99@gmail.com>
2025-03-05 14:36:33 +0400

    Les boutons de filtre sont accessible avec le clavier

--- assets/maugallery.js
+++ assets/maugallery.js
@@ -220,9 +220,9 @@
     },
     showItemTags(gallery, position, tags) {
       var tagItems =
-        '<li class="nav-item"><span class="nav-link active active-tag"  data-images-toggle="all">Tous</span></li>';
+        '<li class="nav-item" tabindex=0><span class="nav-link active active-tag"  data-images-toggle="all">Tous</span></li>';
       $.each(tags, function(index, value) {
-        tagItems += `<li class="nav-item active">
+        tagItems += `<li class="nav-item active" tabindex=0>
                 <span class="nav-link"  data-images-toggle="${value}">${value}</span></li>`;
       });
       var tagsRow = `<ul class="my-4 tags-bar nav nav-pills">${tagItems}</ul>`;
